e70c593b1ef3915304a730d9a941fea3ccaccf8c
baldeweb <wallacecorin@gmail.com>
2018-04-13 09:13:41 -0300

    Update Helper.java
    inclusão dos metodos de show keyboard and hide keyboard

--- Android/Classes/Helper.java
+++ Android/Classes/Helper.java
@@ -107,5 +107,26 @@ public class Helper {
         if (intent.resolveActivity(ctx.getPackageManager()) != null) {
             ctx.startActivity(intent);
         }
+    }   
+    
+    public static void showSoftInput(EditText edit, Context context) {
+        edit.setFocusable(true);
+        edit.setFocusableInTouchMode(true);
+        edit.requestFocus();
+        InputMethodManager imm = (InputMethodManager) context
+                .getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.showSoftInput(edit, 0);
+        }
+    }
+    
+    public static void hideSoftInput(Activity activity) {
+        View view = activity.getCurrentFocus();
+        if (view == null) view = new View(activity);
+        InputMethodManager imm = (InputMethodManager) activity
+                .getSystemService(Activity.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
